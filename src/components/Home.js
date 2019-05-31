411ec292cbd99ac3b0c0d7c586e28b6c18fb1755
Dat Nguyen <Dat_T_Nguyen@live.com>
2019-05-30 20:27:25 -0400

    homepage edits

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,19 +5,31 @@ import ReactFullpage from '@fullpage/react-fullpage';
 import aboutBg from '../assets/about-bg.jpg';
 
 class Home extends Component {
+  handleScroll = () => {
+    let scroll = document.getElementById('scroll');
+
+    if (scroll.style.visibility === 'hidden') {
+      setTimeout(() => {
+        scroll.style.visibility = 'visible';
+      }, 1000);
+    } else {
+      scroll.style.visibility = 'hidden';
+    }
+  };
   render() {
     return (
       <>
         <Navbar />
         <ReactFullpage
           scrollingSpeed={1000}
+          onLeave={this.handleScroll}
           render={() => {
             return (
               <ReactFullpage.Wrapper>
                 <div id="about" className="section">
                   <img src={aboutBg} alt="background" />
                   <h1>The Garden</h1>
-                  <div className="scroll" />
+                  <div id="scroll" />
                 </div>
                 <ProductsHome />
               </ReactFullpage.Wrapper>
